a78c3f16e122de3c93f33aa1f268d18969773310
Joseph Opio <josephopio11@gmail.com>
2025-08-12 11:26:44 +0800

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,60 @@
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Image } from "expo-image";
+import { Fragment } from "react";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className={"h-full w-1/2"}>
+                      <Image
+                        source={item.image}
+                        className={"size-full"}
+                        contentFit="contain"
+                      />
+                    </View>
+
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        contentFit="contain"
+                        tintColor="#ffffff"
+                      />
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
+  );
+}
---
+++ components/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Test = () => {
+  return (
+    <View>
+      <Text className="font-bold uppercases">Test</Text>
+    </View>
+  );
+};
+
+export default Test;
